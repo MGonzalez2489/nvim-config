20e324a808d8ab1ed2e27d31de3f5a5690db642b
ManuelGonzalez <manuel_agh@hotmail.com>
2025-08-20 14:08:31 -0600

    delete mason workaround

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,3 @@
 return {
-  -- { "folke/trouble.nvim", enabled = false },
   { "zbirenbaum/copilot", enabled = false },
-  { "tokyonight.nvim", enabled = false },
-  -- { "nvim-lint", enabled = false },
 }
