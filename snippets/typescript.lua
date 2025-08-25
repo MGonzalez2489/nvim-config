175fa9119ad816aafc15685df9b85e0e74d2bedf
ManuelGonzalez <manuel_agh@hotmail.com>
2025-08-25 09:42:17 -0600

    react snippets

--- snippets/typescript.lua
+++ snippets/typescript.lua
@@ -3,7 +3,6 @@ local ls = require("luasnip")
 local t = ls.text_node
 
 return {
-  ls.snippet({ trig = "hit" }, { t("wasaaa ts bitch!") }),
   ls.snippet({ trig = "cl" }, {
     t("console.log("),
     i(1), -- El cursor se posicionará aquí
