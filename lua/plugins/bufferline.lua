d095089777958f4d479305f55a3af0128b0e4ae6
ManuelGonzalez <manuel_agh@hotmail.com>
2026-08-23 17:44:15 -0600

    bufferline enhancements

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,10 +5,53 @@ return {
 
     opts = {
       options = {
-        mode = "tabs",
-        -- show_buffer_close_icons = false,
-        -- show_close_icon = false,
+      -- stylua: ignore
+      close_command = function(n) Snacks.bufdelete(n) end,
+      -- stylua: ignore
+      right_mouse_command = function(n) Snacks.bufdelete(n) end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = true,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = LazyVim.config.icons.diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+          {
+            filetype = "snacks_layout_box",
+          },
+        },
+        ---@param opts bufferline.IconFetcherOpts
+        get_element_icon = function(opts)
+          return LazyVim.config.icons.ft[opts.filetype]
+        end,
       },
     },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+
+    -- opts = {
+    --   options = {
+    --     mode = "tabs",
+    --     -- show_buffer_close_icons = false,
+    --     -- show_close_icon = false,
+    --   },
+    -- },
   },
 }
